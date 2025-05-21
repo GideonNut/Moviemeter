4f190e8b56023e2e9df6d9f4d547ce29d136f166
Gideonnut <ngideon538@yahoo.com>
2025-05-21 12:19:20 +0100

    update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,25 +1,18 @@
 "use client"
 
 import type React from "react"
-import { WagmiProvider, createConfig } from "wagmi"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { frameConnector } from "@farcaster/frame-wagmi-connector"
-import { mainnet } from "viem/chains"
+import { ThirdwebProvider } from "thirdweb/react"
 import { MovieProvider } from "@/lib/state/MovieContext"
-
-const config = createConfig({
-  chains: [mainnet],
-  connectors: [frameConnector()],
-})
-
-const queryClient = new QueryClient()
+import { celo } from "thirdweb/chains"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <MovieProvider>{children}</MovieProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ThirdwebProvider
+      supportedChains={[celo]}
+    >
+      <MovieProvider>
+        {children}
+      </MovieProvider>
+    </ThirdwebProvider>
   )
 }
