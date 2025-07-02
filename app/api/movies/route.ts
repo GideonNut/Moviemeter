a779ad96a55fde96064c9225e0210e62860dfa8e
Gideonnut <ngideon538@yahoo.com>
2025-07-02 17:36:06 +0100

    feat: create route.ts

---
+++ app/api/movies/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server"
+import { connectToDatabase } from "../../../lib/mongodb"
+import Movie from "../../../models/Movie"
+
+export async function GET() {
+  await connectToDatabase()
+  const movies = await Movie.find()
+  return NextResponse.json(movies)
+}
+
+export async function POST(req: NextRequest) {
+  await connectToDatabase()
+  const data = await req.json()
+  const movie = await Movie.create(data)
+  return NextResponse.json(movie)
+}
