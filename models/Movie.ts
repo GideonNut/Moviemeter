bd4672b1b58c99541bdc0ff6cb8db5fb22535d97
Gideonnut <ngideon538@yahoo.com>
2025-08-12 18:42:49 +0100

    update Movie.ts

--- models/Movie.ts
+++ models/Movie.ts
@@ -4,6 +4,7 @@ const MovieSchema = new Schema({
   title: String,
   description: String,
   posterUrl: String,
+  isTVSeries: { type: Boolean, default: false },
 }, { timestamps: true })
 
 export default models.Movie || model("Movie", MovieSchema)
