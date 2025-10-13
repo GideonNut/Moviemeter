cf6542594ddf3e393bef12f8a715886fa128e2d0
Gideonnut <ngideon538@yahoo.com>
2025-10-13 16:56:37 +0100

    update loading.tsx

--- app/tv/loading.tsx
+++ app/tv/loading.tsx
@@ -61,14 +61,11 @@ export default function TVLoading() {
           ))}
         </div>
 
-        {/* Pagination Skeleton */}
-        <div className="flex justify-center mt-8">
-          <div className="flex gap-2">
-            <Skeleton className="h-10 w-10" />
-            <Skeleton className="h-10 w-10" />
-            <Skeleton className="h-10 w-10" />
-            <Skeleton className="h-10 w-10" />
-            <Skeleton className="h-10 w-10" />
+        {/* Infinite scroll loading indicator */}
+        <div className="flex justify-center py-8">
+          <div className="flex items-center gap-2 text-zinc-400">
+            <div className="w-6 h-6 border-2 border-zinc-600 border-t-zinc-400 rounded-full animate-spin"></div>
+            <span>Loading more TV shows...</span>
           </div>
         </div>
       </div>
