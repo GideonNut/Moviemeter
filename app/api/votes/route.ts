7e1e86b65a10f45e5838948142f34bda3a203a91
Gideonnut <ngideon538@yahoo.com>
2025-12-17 19:19:51 +0000

    update route.ts

--- app/api/votes/route.ts
+++ app/api/votes/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { connectToDatabase } from "../../../lib/mongodb"
+import { connectToDatabase } from "@/lib/db/mongodb"
 import Vote from "../../../models/Vote"
 
 export async function GET(req: NextRequest) {
