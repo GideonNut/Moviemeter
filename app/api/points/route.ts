d441ddfc4080576e69d1d3fe20d26d7bbf40e398
Gideonnut <ngideon538@yahoo.com>
2025-12-17 19:18:14 +0000

    update route.ts

--- app/api/points/route.ts
+++ app/api/points/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { connectToDatabase } from "@/lib/mongodb"
+import { connectToDatabase } from "@/lib/db/mongodb"
 import User from "@/models/User"
 
 export async function POST(req: NextRequest) {
