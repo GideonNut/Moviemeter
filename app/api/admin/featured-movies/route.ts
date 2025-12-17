3391e14b9836171c62825c19f9f50a72298a8350
Gideonnut <ngideon538@yahoo.com>
2025-12-17 19:17:02 +0000

    update route.ts

--- app/api/admin/featured-movies/route.ts
+++ app/api/admin/featured-movies/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { connectToDatabase } from "@/lib/mongodb"
+import { connectToDatabase } from "@/lib/db/mongodb"
 import FeaturedMovie from "@/models/FeaturedMovie"
 
 export async function GET() {
