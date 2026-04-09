3fe06de39e88164246c0cc3990d991450d9fb0ca
Gideonnut <ngideon538@yahoo.com>
2026-04-09 16:56:51 +0100

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="talentapp:project_verification"
+          content="36e39149fe5b5e09fed2e247875ce5166dbbac1ace8f51586dbfce0e6a3836a1c5c381ba9a2b8555c61193f4e82972ff4e129b60e69ec36e20cf180252a049ec"
+        />
+      </head>
       <body className={`${inter.className} bg-background text-foreground antialiased`}>
         <Providers>
           {children}
